598bfff3ad3cb416495d565776bc1000da5e18eb
unknown <eduardo.vidal@sempreceub.com>
2023-05-24 21:59:52 -0300

    trabalho

--- .github/jogo da adivinhacao.go
+++ .github/jogo da adivinhacao.go
@@ -7,11 +7,13 @@ import (
 
 func main() {
 	var JogarNovamente string
-	tentativasTotais := 0
+	tentativasTotais := []int{}
 	var NumeroAdivinhado int
-	fmt.Println("Bem vindo ao jogo da adivinhação.")
+
+	fmt.Println("Bem vindo ao jogo da adivinhação. ")
 
 	for {
+
 		NumeroAleatorio := rand.Intn(99) + 1
 		tentativas := 0
 		fmt.Print("Tente adivinhar o numero aleatorio gerado. Digite um numero inteiro entre 1 e 100: ")
@@ -22,26 +24,30 @@ func main() {
 				fmt.Print("tente novamente: ")
 				fmt.Scan(&NumeroAdivinhado)
 				tentativas++
-				tentativasTotais++
 
 			} else if NumeroAleatorio < NumeroAdivinhado {
 				fmt.Println("o numero é menor que", NumeroAdivinhado)
 				fmt.Print("tente novamente: ")
 				fmt.Scan(&NumeroAdivinhado)
 				tentativas++
-				tentativasTotais++
+
 			} else if NumeroAleatorio == NumeroAdivinhado {
 				fmt.Println("Parabens você acertou!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 				tentativas++
-				tentativasTotais++
 				break
 			}
 
 		}
-		fmt.Printf("voce fez %d tentativas", tentativas)
-		fmt.Println("você deseja jogar novamente?(s/n)")
+		tentativasTotais = append(tentativasTotais, tentativas)
+		fmt.Printf("voce precisou de %d chutes.", tentativas)
+
+		fmt.Println(" deseja jogar novamente?(s/n)")
 		fmt.Scan(&JogarNovamente)
 		if JogarNovamente == "n" {
+			for i := 0; i < len(tentativasTotais); i++ {
+
+				fmt.Printf("voce fez %d chutes na tentativa %d . ", tentativasTotais[i], i+1)
+			}
 			break
 		}
 	}
